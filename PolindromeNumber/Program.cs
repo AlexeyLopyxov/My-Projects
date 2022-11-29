d7cdfa86e147e90051c8ca37a221dd7c81ec4961
AlexeyLopuxov <alexeylopyxov00@gmail.com>
2022-11-29 23:21:52 +0500

    Polindrom Number

---
+++ PolindromeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Polindrom
+{
+    static void Main()
+    {
+        Console.Write("Input an integer number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0, temp;
+        int x = 0;
+
+        temp = num;
+
+        Console.WriteLine("Число: " + num);
+
+        while (num>0)
+        {
+            x = num % 10;
+            num = num / 10;
+            sum = sum * 10 + x;
+        }
+        Console.WriteLine("Число наоборот: " + sum);
+        
+        if (temp == sum)
+            Console.WriteLine("Polindrome");
+        else 
+            Console.WriteLine("No polindrome");
+    }
+}
